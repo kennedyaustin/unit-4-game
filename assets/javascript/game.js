48b24e802ff09e7c84f4f73387e8e1d2631c57cd
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-04-24 12:38:36 -0400

    Did psuedo code in comments so that I can hopefully finish this a bit easier

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+// set some Global variables
+
+// Reset function to reset all variables
+
+  // Have to reset chosenCharacters att and hp stats
+  // Hide chosen character and enemy characters
+  // Reset the hp of all characters
+  // Reset the attack power of all characters
+  // Delete the text that shows after an action has been done
+  // Put the pictures back in the character select screen
+
+// A variable to hold all of the stats for the characters
+
+// When the player clicks on one of the characters, the site will take whichever character
+// was chosen and move it to the Your Character section while moving the other 3 into the 
+// Enemies section
+
+// When the enemies move sections need to change background color and border color
+
+// Clear the character select section
+
+// If a player clicks on one of the enemies in the Enemies section it will move the chosen
+// enemy and move them to the Defender section, other characters stay in their section
+
+// Code the attack button
+    // As you attack more, CharAtt goes up
+    // When the attack button is hit, your character hits first, and then the enemy
+    // Enemyhp-CharAtt, vice versa
+    // If Charhp<1 I lose, if enemyhp<1 I win, chose next enemy
+    // Show message that the player has beaten their selected enemy
+    // If no enemy is there, display the message that there's no one to attack
+//
+
+// If all enemies have been defeated and charhp>1 the player wins
+// Clear the text from the fights and tell the player they won
+// Show the restart button underneath the text

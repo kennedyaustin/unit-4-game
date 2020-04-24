edd40f8d2ac90f810e05f96f1cc5bf161174f963
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-04-24 13:22:39 -0400

    added some global variables to the javascript doc, also made an object with each characters stats to make it easier to ref

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,58 @@
 // set some Global variables
+let attack;
+let CharacterAttack;
+let CharacterHP;
+let CharacterAttackVal;
+let defendingCharacter;
+let defendingCharacterHP;
+let defendingCharacterAttackVal;
 
-// Reset function to reset all variables
+// An object to hold all of the stats for the characters so that they're easier to 
+// reference in jQuery
+let characters= {
 
-  // Have to reset chosenCharacters att and hp stats
-  // Hide chosen character and enemy characters
-  // Reset the hp of all characters
-  // Reset the attack power of all characters
-  // Delete the text that shows after an action has been done
-  // Put the pictures back in the character select screen
+    Anakin: {
+        divName: "Anakin",
+        Name: "Anakin Skywalker",
+        visual: "../images/anakin.jpg",
+        HP: 100,
+        attackingPower: 10,
+        counter: 5
+    },
+    ObiWan: {
+        divName: "ObiWan",
+        Name: "Obi-Wan Kenobi",
+        visual: "../images/obi-wan.jpg",
+        HP: 120,
+        attackingPower: 8,
+        counter: 15
+    },
+    DarthSidious: {
+        divName: "darthSidious",
+        Name: "Darth Sidious",
+        visual: "../images/darthSidious.jpg",
+        HP: 150,
+        attackingPower: 5,
+        counter: 20
+    },
+    highGround: {
+        divName: "highGround",
+        Name: "I HAVE THE HIGH GROUND ANAKIN",
+        visual: "../images/highGround.jpg",
+        HP: 180,
+        attackingPower: 5,
+        counter: 25
+    }
+}
 
-// A variable to hold all of the stats for the characters
+$(document).ready(function() {
 
 // When the player clicks on one of the characters, the site will take whichever character
 // was chosen and move it to the Your Character section while moving the other 3 into the 
 // Enemies section
+
+
+})
 
 // When the enemies move sections need to change background color and border color
 
@@ -34,3 +73,13 @@
 // If all enemies have been defeated and charhp>1 the player wins
 // Clear the text from the fights and tell the player they won
 // Show the restart button underneath the text
+
+// Reset function to reset all variables
+
+  // Have to reset chosenCharacters att and hp stats
+  // Hide chosen character and enemy characters
+  // Reset the hp of all characters
+  // Reset the attack power of all characters
+  // Delete the text that shows after an action has been done
+  // Put the pictures back in the character select screen
+//
